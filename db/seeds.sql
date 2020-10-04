b54e63fb3b7d63143ecc153a49632c98f49b4895
John Baxter <jbaxter@a-zdarts.com>
2020-10-03 19:52:20 -0700

    got base template working with router assignment for '/'

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Double-Double Animal style');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name) VALUES ('Whopper');
+INSERT INTO burgers (burger_name) VALUES ('Baconator');
+INSERT INTO burgers (burger_name) VALUES ('1/3 lb Thickburger');
